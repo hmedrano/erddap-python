ab0ed69a272d174118d66afe512b1cf4f1417865
Favio Medrano <hmedrano@macbook.pro>
2021-03-20 01:29:40 -0700

    .2b

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="erddap-python",
-    version="0.0.1b",
+    version="0.0.2b",
     author="Favio Medrano",
     author_email="hmedrano@cicese.mx",
     description="Python erddap API client",
